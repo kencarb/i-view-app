8b1ce68d0ae9897a05cbbd244ae15e2bbdd8027a
EC2 Default User <ec2-user@ip-172-31-10-91.ec2.internal>
2021-02-19 23:51:32 +0000

    initial commit

---
+++ src/containers/Settings.js
@@ -0,0 +1,23 @@
+import React from "react"; 
+// import React, { useState, useEffect } from "react";
+// import { useHistory } from "react-router-dom";
+// import { API } from "aws-amplify";
+// import { onError } from "../libs/errorLib";
+// import config from "../config";
+
+export default function Settings() {
+  // const history = useHistory();
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // function billUser(details) {
+  //   return API.post("notes", "/billing", {
+  //     body: details
+  //   });
+  // }
+
+  return (
+    <div className="Settings">
+      <span role="img" aria-label="construction">🚧</span> Billing page "Under Construction" <i>...free for now.</i> <span role="img" aria-label="construction">🚧</span>
+    </div>
+  );
+}
